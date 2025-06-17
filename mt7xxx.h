499b84843afbdeb0b843a96ac27eb99b478dc15b
Eric Woudstra <ericwouds@gmail.com>
2025-06-17 20:17:31 +0200

    Update mt7xxx.h double mmc

--- mt7xxx.h
+++ mt7xxx.h
@@ -31,6 +31,8 @@
 
 #define BOOT_TARGET_DEVICES(func)	\
 	func(MMC, mmc, 1)	\
+	func(MMC, mmc, 1)	\
+	func(MMC, mmc, 0)	\
 	func(MMC, mmc, 0)	\
 	func(NVME, nvme, 0)	\
 	func(UBIFS, ubifs, 0, ubi, rootfs)
